d685f07459cdc51c59db7623a071ba95d0e4537e
Mark Dessain <mark.dessain@gmail.com>
2024-09-07 16:01:03 +0100

    update kube config and add service

--- packages/kubernetes.nix
+++ packages/kubernetes.nix
@@ -13,6 +13,10 @@ pkgs.stdenv.mkDerivation rec {
       ln -s ${pkgs.kubernetes-helm}/bin/helm $out/bin/helm
       ln -s ${pkgs.kind}/bin/kind $out/bin/kind
 
+      echo 'mkdir -p ~/.config/kube' > $out/.env
+      echo 'if [ -z "$( ls -A ~/.config/kube )" ]; then echo "No kubernetes configs found"; else export KUBECONFIG=$(for filename in ~/.config/kube/*; do echo -n "$filename,"; done | sed "s/,$//"); fi' >> $out/.env 
+      chmod +x $out/.env 
+
       echo 'kubectl exec svc/$1 -c $1 -it -- ''${2:-bash}' > $out/bin/pod_view 
       chmod +x $out/bin/pod_view 
 
--- service.sh
+++ service.sh
@@ -1,3 +1,20 @@
+
+if [[ "$1" == "alpha" ]]
+then
+    echo "Setup alpha shell"
+elif [[ "$1" == "beta" ]]
+then
+    echo "Setup beta shell"
+else
+    echo "Please select: [alpha|beta]"
+    exit 1
+fi
+
+
+SHELL_NAME=$1
+NIX_REPO_ROOT=$(dirname $(readlink -f $0))
+
+cat <<EOT >> /lib/systemd/system/nix-code-server@.service
 [Unit]
 Description=code-server
 After=network.target
@@ -5,9 +22,11 @@ After=network.target
 [Service]
 Type=exec
 ExecStart=/nix/var/nix/profiles/default/bin/nix-shell --pure --command "zsh -c 'code'"
-WorkingDirectory=/home/pi/projects/nix/alpha
+WorkingDirectory=$NIX_REPO_ROOT/$SHELL_NAME
 Restart=always
 User=%i
 
 [Install]
 WantedBy=default.target
+
+EOT
