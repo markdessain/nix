0e14823dafaf7d9d4a649de7571c8e93bb5000e7
Mark Dessain <mark.dessain@gmail.com>
2025-01-24 06:43:59 +0000

    upgrade to later version of nixpkgs

--- packages/ai.nix
+++ packages/ai.nix
@@ -4,10 +4,11 @@ pkgs.stdenv.mkDerivation rec {
     pname = "ai";
     version = "0.1.0";
     phases = [ "installPhase" ];
-
+    
+    # ln -s ${pkgs.open-webui}/bin/open-webui $out/bin/open-webui
+    
     installPhase = ''
       mkdir -p $out/bin
-      ln -s ${pkgs.open-webui}/bin/open-webui $out/bin/open-webui
       ln -s ${pkgs.ollama}/bin/ollama $out/bin/ollama
       ln -s ${pkgs.openai-whisper}/bin/whisper $out/bin/whisper
     '';
--- packages/vscode.nix
+++ packages/vscode.nix
@@ -9,19 +9,19 @@ unFreePkgs.stdenv.mkDerivation rec {
       vscodeExtensions = with unFreePkgs.vscode-extensions; []
       ++ unFreePkgs.vscode-utils.extensionsFromVscodeMarketplace [
         { publisher = "pomdtr"; name = "excalidraw-editor";version = "3.7.4"; sha256 = "sha256-hI+Qo8K+gLQuzKkaSq89D8vIxlYq9tMi31DgFiRzx0E="; }
-        { publisher = "golang"; name = "go"; version = "0.41.4"; sha256 = "sha256-ntrEI/l+UjzqGJmtyfVf/+sZJstZy3fm/PSWKTd7/Q0="; }
-        { publisher = "ms-azuretools"; name = "vscode-docker"; version = "1.29.1"; sha256 = "sha256-uleZLIkfOxKan+U56fKEwUjaleig07zdEwQKzsAzan0="; }
-        { publisher = "ms-toolsai"; name = "jupyter"; version = "2024.3.0"; sha256 = "sha256-m8sJ3c34lrV5c484s+wlOpQc8IWhDvhlMbWCJeo0en4="; }
+        { publisher = "golang"; name = "go"; version = "0.45.0"; sha256 = "sha256-w/74OCM1uAJzjlJ91eDoac6knD1+Imwfy6pXX9otHsY="; }
+        { publisher = "ms-azuretools"; name = "vscode-docker"; version = "1.29.4"; sha256 = "sha256-j2ACz2Ww5hddoDLHGdxnuQIqerP5ogZ80/wS+Aa5Gdo="; }
+        { publisher = "ms-toolsai"; name = "jupyter"; version = "2024.9.1"; sha256 = "sha256-7ZNTIymw0LiM5TmDOxrwcyJGhxjdYuvaC4+Tb+YSASs="; }
         { publisher = "ms-python"; name = "python"; version = "2024.4.1"; sha256 = "sha256-n1zNxUV7vfVcHY0jjWtwBVzZkgjZipwMT5vSubuNtZw="; }
         { publisher = "evilz"; name = "vscode-reveal"; version = "4.3.3"; sha256 = "sha256-KqvQi0DMfHppX96qKHIkO9zIueBdGGV+6dYkpFEzFBo="; }
-        { publisher = "ms-python"; name = "debugpy"; version = "2024.8.0"; sha256 = "sha256-gvuYfyVIehy3ItJVfQbL/d8gO2KOl2rg9pBrmJuZeME="; }
-        { publisher = "ms-toolsai"; name = "jupyter-renderers"; version = "1.0.18"; sha256 = "sha256-4EKXaWsafonBK7TftjVUBK3ujqD+OguXV0y/bWoWTyk="; }
+        { publisher = "ms-python"; name = "debugpy"; version = "2024.15.2025012001"; sha256 = "sha256-wiN+jHAZuzcHk5VRoQJeWgQf4LzTx5J7Nu0PdNuwzAY="; }
+        { publisher = "ms-toolsai"; name = "jupyter-renderers"; version = "1.0.19"; sha256 = "sha256-15333GNQZhuJGOskz0FEi3mTdGO8ocfYpfZyyUbGYbM="; }
         { publisher = "ms-toolsai"; name = "jupyter-keymap"; version = "1.1.2"; sha256 = "sha256-9BLyBZzZ0Z6QQ05QSxFJYNZmZDc5O3eYkCxe/UsmKws="; }
         { publisher = "ms-toolsai"; name = "vscode-jupyter-cell-tags"; version = "0.1.9"; sha256 = "sha256-XODbFbOr2kBTzFc0JtjiDUcCDBX1Hd4uajlil7mhqPY="; }
-        { publisher = "streetsidesoftware"; name = "code-spell-checker"; version = "4.0.13"; sha256 = "sha256-8VYg7NAFNW2aLZD0q25fgcrBfU1ptWF7Fy7I4SizTGc="; }
-        { publisher = "rjmacarthy"; name = "twinny"; version = "3.17.9"; sha256 = "sha256-ARJNGthnXKxaSE6ultQ7qrhwy6zSmanEAn6vk18Nmi0="; }
-        { publisher = "Evidence"; name = "evidence-vscode"; version = "1.5.1"; sha256 = "sha256-UWuAvn0pU7pW7xIQJA1zfop3N6bGrdGX2xE8thPbqh0="; }
-        { publisher = "svelte"; name = "svelte-vscode"; version = "109.0.2"; sha256 = "sha256-FrGvQ074iaHxZukB0bL6rNkdB5UpZJ5yp4wnmlRJA+g="; }
+        { publisher = "streetsidesoftware"; name = "code-spell-checker"; version = "4.0.35"; sha256 = "sha256-MfGlqOvfPK13Paoug3lSsdslqgbypuqvdqm9bagu1NY="; }
+        { publisher = "rjmacarthy"; name = "twinny"; version = "3.21.14"; sha256 = "sha256-M0GcYRNEiSQS3cfFche2olYHr7kC+Pm8U5q6+voAV9c"; }
+        { publisher = "Evidence"; name = "evidence-vscode"; version = "1.5.6"; sha256 = "sha256-uRiv3aarSeY8kHYeKYQzhDCEn16ZErgKQTUGoZtFuPc="; }
+        { publisher = "svelte"; name = "svelte-vscode"; version = "109.5.2"; sha256 = "sha256-y1se0+LY1M+YKCm+gxBsyHLOQU6Xl095xP6z0xpf9mM="; }
       ];
     };
 
@@ -39,8 +39,9 @@ unFreePkgs.stdenv.mkDerivation rec {
         echo "" >> $out/bin/code2
       fi
 
-      #cat ${vcsodeWithExtension}/bin/code | sed 's,${unFreePkgs.vscode},${unFreePkgs.vscodium},g' | sed 's,/bin/code,/bin/codium,g' >> $out/bin/code2
-      cat ${vcsodeWithExtension}/bin/code >> $out/bin/code2
+      # Choose to use vscodium or vscode
+      cat ${vcsodeWithExtension}/bin/code | sed 's,${unFreePkgs.vscode},${unFreePkgs.vscodium},g' | sed 's,/bin/code,/bin/codium,g' >> $out/bin/code2
+      #cat ${vcsodeWithExtension}/bin/code >> $out/bin/code2
       chmod +x $out/bin/code2
 
       mkdir -p $out/config
