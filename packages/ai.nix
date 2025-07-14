39181fd7f32d1ceac294f797e7e57a95976a68d9
Mark Dessain <mark.dessain@gmail.com>
2025-07-14 13:30:45 +0100

    allow opencode multi environments

--- packages/ai.nix
+++ packages/ai.nix
@@ -24,11 +24,16 @@ pkgs.stdenv.mkDerivation rec {
         ln -s /opt/homebrew/bin/tabby $out/bin/tabby
         ln -s /opt/homebrew/bin/llama-server $out/bin/llama-server
       elif [[ "${system}" == "aarch64-linux" ]]; then         
-
+        ln -s /nix/store/lb8a87ynrji2b0i0i7bwp1jjwzav8maq-opencode-0.1.194/bin/opencode $out/bin/opencode
         ln -s /nix/store/kakd108mxvgr5kcbxksq3qschqs4fnya-gemini-cli-0.1.5/bin/gemini $out/bin/gemini
         echo 'cd ~/projects/tools && cd $(${pkgs.gum}/bin/gum choose $(ls)) && /nix/store/kakd108mxvgr5kcbxksq3qschqs4fnya-gemini-cli-0.1.5/bin/gemini --sandbox-image gemini --yolo' > $out/bin/gemini-tools 
         chmod +x $out/bin/gemini-tools
+
       fi 
 
+      echo 'cd ~/projects/tools && cd $(${pkgs.gum}/bin/gum choose $(ls)) && docker run -it -v "~/.local/share/opencode:/root/.local/share/opencode" -v "~/.config/opencode:/ot/.config/opencode" -v "~/.local/state/opencode:/root/.local/state/opencode" -v $(pwd):/project --workdir /project opencode ' > $out/bin/opencode-tools 
+      chmod +x $out/bin/opencode-tools
+      echo 'docker run -it -v "~/.local/share/opencode:/root/.local/share/opencode" -v "~/.config/opencode:/ot/.config/opencode" -v "~/.local/state/opencode:/root/.local/state/opencode" -v $(pwd):/project --workdir /project opencode ' > $out/bin/opencode-project 
+      chmod +x $out/bin/opencode-project
     '';
 }
