34223baa897f71904aa9fa1e7e842d02c8c5b6c1
Mark Dessain <mark.dessain@gmail.com>
2026-09-03 11:38:52 +0200

    adding brawp

--- packages/linux.nix
+++ packages/linux.nix
@@ -5,6 +5,11 @@ pkgs.stdenv.mkDerivation rec {
     version = "0.1.0";
     phases = [ "installPhase" ];
 
+    zed = pkgs.fetchzip {
+      url = "https://cloud.zed.dev/releases/stable/latest/download?asset=zed&arch=aarch64&os=linux&source=docs.tar.gz";
+      sha256 = "sha256-xUNHx124Yuk1Iai0OYFS88ds7KXbgihcoP4GjfpURiQ=";
+    };
+
     installPhase = ''
       mkdir -p $out/bin
       ln -s ${pkgs.ddcutil}/bin/ddcutil $out/bin/ddcutil
@@ -14,5 +19,14 @@ pkgs.stdenv.mkDerivation rec {
       ln -s ${pkgs.gccgo15}/bin/cc $out/bin/cc
       ln -s ${pkgs.gccgo15}/bin/gcc $out/bin/gcc
       ln -s ${pkgs.perl}/bin/perl $out/bin/perl
+      ln -s ${pkgs.bubblewrap}/bin/bwrap $out/bin/bwrap
+
+      # WAYLAND_DISPLAY="" ${zed}/bin/zed "$@"
+      cat << 'EOF' > $out/bin/zed
+      #!/bin/sh
+      WAYLAND_DISPLAY="" ZED_BACKEND=vulkan ${zed}/bin/zed "$@"
+
+      EOF
+      chmod +x $out/bin/zed
     '';
 }
